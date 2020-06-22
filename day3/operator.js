4636424cd861ae9c53007f8e1ade6af44511665f
Heesu Yeom <43159337+yeom-heesu@users.noreply.github.com>
2020-06-22 16:10:44 +0900

    Add files via upload

---
+++ day3/operator.js
@@ -0,0 +1,16 @@
+// 1. String concatenation
+console.log('my'+'cat');
+//문자열 + 문자열 가능
+
+console.log('1'+2);
+// string 을 int로 변환하여 계산
+
+console.log(`string literals: 1 + 2 = ${1+2}`);
+
+//2. Numeric operatiors
+//일반적인 숫자 계산방법과 같다
+
+//3. Increment and decrement operators
+//증감연산
+let counter = 2;
+const preIncrement = ++counter;
